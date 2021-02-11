e02af43db3878699569eb375f0c898fbd4569672
overloadcode <prywatnaapka@gmail.com>
2021-02-11 19:13:02 +0100

    main page view update #2 (add popupmenu, changes in structure)

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -5,7 +5,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notebook/app.dart';
 import 'package:notebook/data/resources/notebook_local_db_impl.dart';
 
-
 import 'package:notebook/presentation/blocs/book_bloc.dart';
 import 'package:notebook/presentation/widgets/add_item_btn.dart';
 
@@ -16,61 +15,93 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => BookBloc(NotebookLocalDbImpl()),
-      child: NewWidget(),
+      child: MainLayout(),
     );
   }
 }
 
-class NewWidget extends StatelessWidget {
-  const NewWidget({
+class MainLayout extends StatelessWidget {
+  final TextEditingController renameBookController = TextEditingController();
+
+  MainLayout({
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('main'),
-        ),
-        body: Content(),
-        floatingActionButton: AddItemBtn(onPressed: () {
-    final book = BookTableCompanion(name: Value<String>('s'));
-    context.read<BookBloc>().add(AddingNewBook(book));
-    }),
+      appBar: AppBar(
+        title: Text('main'),
+      ),
+      body: _Body(),
+      floatingActionButton: AddItemBtn(onPressed: () {
+        final book = BookTableCompanion(name: Value<String>('s'));
+        context.read<BookBloc>().add(AddingNewBook(book));
+      }),
     );
   }
 }
 
-class Content extends StatelessWidget {
-  const Content({
+class _Body extends StatelessWidget {
+  const _Body({
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 0.9,
-      child: FutureBuilder(
+    return FutureBuilder(
         future: context.watch<BookBloc>().getAllBooks,
         builder: (BuildContext context, AsyncSnapshot<List<Book>> snapshot) {
-          if (snapshot.hasError || !snapshot.hasData) {
-            return CircularProgressIndicator();
-          } else if (snapshot.connectionState == ConnectionState.done &&
-              snapshot.hasData) {
-            return BlocBuilder<BookBloc, BookState>(
-                builder: (BuildContext context, BookState builder) {
-              return Container(
-                child: ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(title: Text(snapshot.data[index].name));
-                    }),
-              );
-            });
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return CircularProgressIndicator();
+            default:
+              if (snapshot.hasError) {
+                return CircularProgressIndicator();
+              } else {
+                return _BookListView(bookList: snapshot.data);
+              }
           }
         },
-      ),
-    );
+      );
+
+  }
+}
+
+class _BookListView extends StatelessWidget {
+  final List<Book> bookList;
+  const _BookListView({
+    this.bookList,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BookBloc, BookState>(
+        builder: (BuildContext context, BookState builder) {
+      return ListView.builder(
+          itemCount: bookList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(bookList[index].name),
+              trailing: PopupMenuButton<String>(
+                onSelected: (action) {
+                  if(action == 'Delete') {
+                    context.read<BookBloc>().add(RemoveBook(bookList[index]));
+                  }
+                },
+                itemBuilder: (BuildContext context) {
+                  return <String>['Rename', 'Delete'].map((e) {
+                    return PopupMenuItem<String>(
+                      value: e,
+                      child: Text(e),
+                  );
+                  }).toList();
+                },
+              ),
+            );
+          });
+    });
   }
 }
 
