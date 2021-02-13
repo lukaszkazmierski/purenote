a138a7eb0d326236c7cbfe1669810604fbc41046
overloadcode <prywatnaapka@gmail.com>
2021-02-13 10:14:55 +0100

    repaired dialog position on soft keyboard

--- lib/core/utils/ui/dialogs/book_name_form_dialog.dart
+++ lib/core/utils/ui/dialogs/book_name_form_dialog.dart
@@ -1,52 +1,79 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notebook/data/resources/moor_config/moor_database.dart';
+import 'package:notebook/presentation/blocs/book_bloc.dart';
+import 'package:provider/provider.dart';
 
 class BookNameFormDialog extends StatelessWidget {
-  const BookNameFormDialog({Key key}) : super(key: key);
+  final BuildContext contextWithBloc;
+
+  const BookNameFormDialog({@required this.contextWithBloc, Key key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
       type: MaterialType.transparency,
-      child: Stack(
-        clipBehavior: Clip.antiAlias,
-        alignment: Alignment.center,
-        children: <Widget>[
-          FractionallySizedBox(
-            widthFactor: 0.6,
-            heightFactor: 0.25,
-            child: Container(color: Colors.white, child: const _DialogContent()),
-          ),
-        ],
-      ),
+      child: Container(
+        padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: Stack(
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+                height: MediaQuery.of(context).size.height * 0.25,
+                color: Colors.white,
+                child: _DialogContent(contextWithBloc: contextWithBloc))
+          ],
+        ),
+      )
+
     );
   }
 }
 
 class _DialogContent extends StatelessWidget {
-  const _DialogContent({
+  final TextEditingController _nameFieldController = TextEditingController();
+  BuildContext contextWithBloc;
+
+  _DialogContent({
+    this.contextWithBloc,
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FractionallySizedBox(
-        widthFactor: 0.9,
+      widthFactor: 0.9,
       heightFactor: 0.9,
       child: Column(
         children: <Widget>[
           const Text('Creating a new book'),
           TextFormField(
+            controller: _nameFieldController,
             maxLength: 30,
             decoration: const InputDecoration(
               labelText: 'name',
             ),
           ),
-          
           Row(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
               const SizedBox(),
-              RawMaterialButton(onPressed: () {}, child: const Text('Cancel')),
-              RawMaterialButton(onPressed: () {}, child: const Text('Create'))
+              RawMaterialButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
+              RawMaterialButton(
+                  onPressed: () {
+                    final book = BookTableCompanion(
+                        name: Value<String>(_nameFieldController.text));
+                    contextWithBloc.read<BookBloc>().add(AddingNewBook(book));
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Create'))
             ],
           )
         ],
