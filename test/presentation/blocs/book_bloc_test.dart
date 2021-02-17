08b331cb673e2139150ea7ee6ad08088a3b4faa6
overloadcode <prywatnaapka@gmail.com>
2021-02-17 22:23:23 +0100

    book bloc update for named parameter

--- test/presentation/blocs/book_bloc_test.dart
+++ test/presentation/blocs/book_bloc_test.dart
@@ -11,7 +11,7 @@ void main() {
 
   setUp(() {
     notebookLocalDbImplTesting = NotebookLocalDbImplTesting();
-    bookBloc = BookBloc(notebookLocalDbImplTesting);
+    bookBloc = BookBloc(notebookLocalDb:  notebookLocalDbImplTesting);
   });
 
   group('BookBloc tests', () {
