e2e7d3df2ebb0f73431b777bc89685a11b09ac3a
overloadcode <prywatnaapka@gmail.com>
2021-02-19 21:46:59 +0100

    extract widgets to other classes and safe note every 10s

--- lib/presentation/pages/note_edit_page.dart
+++ lib/presentation/pages/note_edit_page.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notebook/presentation/blocs/note_bloc/note_bloc.dart';
 import 'package:notebook/service_locator/service_locator.dart';
+
+TextEditingController _titleTextController;
+TextEditingController _contentTextController;
+
 
 class NoteEditPage extends StatelessWidget {
   final note;
@@ -19,8 +25,7 @@ class NoteEditPage extends StatelessWidget {
 
 class _MainLayoutState extends State<MainLayout> {
   Note note;
-  TextEditingController _titleTextController;
-  TextEditingController _contentTextController;
+
   @override
   void initState() {
     super.initState();
@@ -32,55 +37,15 @@ class _MainLayoutState extends State<MainLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('note edit'),
-      ),
-      body: BlocBuilder<NoteBloc, NoteState>(
-          builder: (BuildContext context, state) {
-        return SingleChildScrollView(
-          physics: const ClampingScrollPhysics(),
-          child: ConstrainedBox(
-          constraints: BoxConstraints(
-          minWidth: MediaQuery.of(context).size.width,
-            minHeight: MediaQuery.of(context).size.height,
-            ),
-          child: IntrinsicHeight(
-            child: FractionallySizedBox(
-                alignment: Alignment.topCenter,
-                widthFactor: 0.95,
-                heightFactor: 0.95,
-                child: Column(
-                  children: [
-                    Expanded(
-                        flex: 1,
-                        child: TextFormField(
-                          controller: _titleTextController,
-                        )),
-                    Expanded(
-                        flex: 12,
-                        child: TextFormField(
-                          controller: _contentTextController,
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Username',
-                          ),
-                          maxLines: null,
-                        )),
-                  ],
-                ),
-            ),
-
-
-            )
-
-
-            ));
-      })
-    );
+        appBar: AppBar(
+          title: Text('note edit'),
+        ),
+        body: BlocBuilder<NoteBloc, NoteState>(
+            builder: (BuildContext context, state) {
+          return _Body(note: note);
+        }));
   }
 }
-
-
 
 class MainLayout extends StatefulWidget {
   final Note note;
@@ -90,3 +55,113 @@ class MainLayout extends StatefulWidget {
   @override
   _MainLayoutState createState() => _MainLayoutState();
 }
+
+class _Body extends StatelessWidget {
+  final Note note;
+
+  const _Body({
+    Key key,
+    @required this.note
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: MediaQuery.of(context).size.width,
+                  minHeight: MediaQuery.of(context).size.height,
+                ),
+                child: IntrinsicHeight(
+                  child: _NoteForm(note: note),
+                ))),
+        onWillPop: () {
+          context.read<NoteBloc>().add(UpdateNote(note.copyWith(
+              title: _titleTextController.text,
+              content: _contentTextController.text)));
+          return Future.value(true);
+        });
+  }
+}
+
+class _NoteFormState extends State<_NoteForm> {
+  final _formKey = GlobalKey<FormState>();
+  Timer updateNoteTimer;
+  Note note;
+
+  @override
+  void initState() {
+    note = widget.note;
+    updateNoteTimer = periodicUpdateNote();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      alignment: Alignment.topCenter,
+      widthFactor: 0.95,
+      heightFactor: 0.95,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Expanded(
+                child: TextFormField(
+                  controller: _titleTextController,
+                  onSaved: onSave,
+                  maxLength: 30,
+
+
+                )),
+            Expanded(
+                flex: 12,
+                child: TextFormField(
+                  controller: _contentTextController,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Your content note',
+                  ),
+                  onSaved: onSave,
+                  maxLines: null,
+                )),
+          ],
+        ),
+      )
+    );
+  }
+
+
+
+  void onSave(String value) {
+    context.read<NoteBloc>().add(UpdateNote(note.copyWith(
+        title: _titleTextController.text,
+        content: _contentTextController.text)));
+  }
+
+  Timer periodicUpdateNote() {
+    return Timer.periodic(const Duration(seconds: 10), (timer) {
+      _formKey.currentState.save();
+    });
+  }
+
+  @override
+  void dispose() {
+    updateNoteTimer.cancel();
+    super.dispose();
+  }
+}
+
+class _NoteForm extends StatefulWidget {
+  final Note note;
+
+  const _NoteForm({@required this.note});
+
+  @override
+  _NoteFormState createState() => _NoteFormState();
+}
+
+
