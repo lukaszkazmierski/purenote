e5bde4225b0ddc55526c410dde3ccb9babe52a8b
overloadcode <prywatnaapka@gmail.com>
2021-02-21 13:24:18 +0100

    implement exceptions in service locator

--- lib/service_locator/service_locator.dart
+++ lib/service_locator/service_locator.dart
@@ -1,4 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:notebook/core/exceptions/exception_code.dart';
+import 'package:notebook/core/exceptions/exception_code_translator.dart';
+import 'package:notebook/core/exceptions/failure.dart';
 import 'package:notebook/data/resources/notebook_local_db_impl.dart';
 import 'package:notebook/domain/repositories/notebook_local_db.dart';
 import 'package:notebook/presentation/blocs/book_bloc/book_bloc.dart';
@@ -12,10 +15,14 @@ class Locator {
    Locator({this.environment = Environment.prod});
 
    T get<T>() => _sl.get<T>();
+   T getWithParam<T>(dynamic param1) => _sl.get<T>(param1: param1);
+
 
    void register() {
     _sl.registerFactory<BookBloc>(() => BookBloc(notebookLocalDb: _sl()));
     _sl.registerFactory<NoteBloc>(() => NoteBloc(notebookLocalDb: _sl()));
+    _sl.registerFactoryParam<Failure, ExceptionCode, void>((ec, _) => Failure(ec));
+    _sl.registerLazySingleton<ExceptionCodeTranslator>(() => ExceptionCodeTranslatorImpl());
     _dbMode();
    }
 
