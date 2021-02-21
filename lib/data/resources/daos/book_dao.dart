3908c998a7781dc6e8dcc6fdfb91c850838f9a24
overloadcode <prywatnaapka@gmail.com>
2021-02-21 16:59:58 +0100

    implementation exception in book dao

--- lib/data/resources/daos/book_dao.dart
+++ lib/data/resources/daos/book_dao.dart
@@ -1,7 +1,11 @@
+import 'package:dartz/dartz.dart';
 import 'package:moor_flutter/moor_flutter.dart';
+import 'package:notebook/core/exceptions/exception_code.dart';
+import 'package:notebook/core/exceptions/failure.dart';
 import 'package:notebook/data/resources/moor_config/moor_database.dart';
 import 'package:notebook/data/resources/sql_tables/book_table.dart';
 import 'package:notebook/data/resources/daos/db_actions.dart';
+import 'package:notebook/service_locator/service_locator.dart';
 
 part 'book_dao.g.dart';
 
@@ -17,10 +21,30 @@ class BookDao extends DatabaseAccessor<MoorDatabase>
   Future<List<Book>> getAllItem() => select(bookTable).get();
   @override
   Stream<List<Book>> watchAllItem([_]) => select(bookTable).watch();
+
   @override
-  Future insertItem(Insertable<Book> note) => into(bookTable).insert(note);
+  Future<Either<Failure, int>> insertItem(Insertable<Book> book) async {
+    try {
+      final insertStatus = await into(bookTable).insert(book);
+      return Right(insertStatus);
+    } on InvalidDataException {
+      final failure = locator.getWithParam<Failure>(ExceptionCodeType.invalidDataException);
+      return Left(failure);
+    } catch(e) {
+      print(e);
+    }
+  }
+
   @override
-  Future updateItem(Insertable<Book> note) => update(bookTable).replace(note);
+  Future<Either<Failure, bool>> updateItem(Insertable<Book> book) async {
+    try {
+      final updateStatus = await update(bookTable).replace(book);
+      return Right(updateStatus);
+    } on InvalidDataException {
+      final failure = locator.getWithParam<Failure>(ExceptionCodeType.invalidDataException);
+      return Left(failure);
+    }
+  }
   @override
   Future deleteItem(Insertable<Book> note) => delete(bookTable).delete(note);
 }
--- lib/data/resources/daos/db_actions.dart
+++ lib/data/resources/daos/db_actions.dart
@@ -1,9 +1,11 @@
+import 'package:dartz/dartz.dart';
 import 'package:moor/moor.dart';
+import 'package:notebook/core/exceptions/failure.dart';
 
 abstract class DbActions<D extends DataClass> {
   Future<List<D>> getAllItem();
   Stream<List<D>> watchAllItem([String bookName]);
-  Future insertItem(Insertable<D> note);
-  Future updateItem(Insertable<D> note);
-  Future deleteItem(Insertable<D> note);
+  Future<Either<Failure, int>> insertItem(Insertable<D> item);
+  Future<Either<Failure, bool>> updateItem(Insertable<D> item);
+  Future deleteItem(Insertable<D> item);
 }
