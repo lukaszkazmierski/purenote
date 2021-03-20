3ef9d26fe28661f417cbab87f89686bac95c8851
overloadcode <prywatnaapka@gmail.com>
2021-03-20 21:54:10 +0100

    intro page added

--- lib/core/utils/routes/router.dart
+++ lib/core/utils/routes/router.dart
@@ -1,11 +1,13 @@
 
 import 'package:auto_route/auto_route_annotations.dart';
 import 'package:notebook/presentation/pages/book_with_notes_pages.dart';
+import 'package:notebook/presentation/pages/intro_page.dart';
 import 'package:notebook/presentation/pages/main_page.dart';
 import 'package:notebook/presentation/pages/note_edit_page.dart';
 
 @MaterialAutoRouter(routes: <AutoRoute>[
-  MaterialRoute(page: MainPage, initial: true),
+  MaterialRoute(page: MainPage),
+  MaterialRoute(page: IntroPage),
   MaterialRoute(page: BookWithNotesPage),
   MaterialRoute(page: NoteEditPage),
 ])
---
+++ lib/presentation/pages/intro_page.dart
@@ -0,0 +1,116 @@
+import 'dart:math' as math show pi;
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart' hide Router;
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:intro_slider/slide_object.dart';
+import 'package:notebook/core/utils/routes/router.gr.dart';
+
+
+class IntroPageState extends State<IntroPage> {
+  List<Slide> slides;
+
+  @override
+  void initState() {
+    super.initState();
+
+    slides = _InitIntroSlides()();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroSlider(
+      slides: slides,
+
+      renderNextBtn: renderNextBtn(),
+      renderSkipBtn: renderSkipBtn(),
+      renderDoneBtn: renderDoneBtn(),
+
+    );
+  }
+
+  Widget renderNextBtn() {
+    return const Icon(
+      Icons.arrow_forward,
+      color: Color(0xff009688),
+      size: 35.0,
+    );
+  }
+
+  Widget renderSkipBtn() {
+    return const Icon(
+      Icons.skip_next,
+      color: Color(0xff009688),
+      size: 35.0,
+    );
+  }
+
+  Widget renderDoneBtn() {
+    return IconButton(
+      icon: const Icon(Icons.done, size: 35.0),
+      color: const Color(0xff009688),
+      onPressed: () => ExtendedNavigator.of(context).pushAndRemoveUntil(Routes.mainPage, (route) => false),
+    );
+  }
+
+}
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key key}) : super(key: key);
+  @override
+  IntroPageState createState() => IntroPageState();
+}
+
+
+
+class _InitIntroSlides {
+  List<Slide> call() {
+    return [
+      Slide(
+        backgroundColor: Colors.white,
+        title: "Purenote",
+        styleTitle: _textTitleStyle(),
+        description:
+        "Take control of your notes",
+        styleDescription: _textDescriptionStyle(),
+        pathImage: "assets/images/icon_app_colored.png",
+      ),
+      Slide(
+        backgroundColor: Colors.white,
+        title: "Actions",
+        styleTitle: _textTitleStyle(),
+        description:
+        "Swipe a single notebook/note to the left to reveal actions",
+        styleDescription: _textDescriptionStyle(),
+        pathImage: "assets/images/swipe_intro.png",
+      ),
+      Slide(
+        backgroundColor: Colors.white,
+        title: "Purenote is under development",
+        maxLineTitle: 2,
+        styleTitle: _textTitleStyle(),
+        description: "The application is constantly being developed with new features.\n"
+            "Keep the app up to date",
+        styleDescription: _textDescriptionStyle(),
+      ),
+
+    ];
+  }
+
+  TextStyle _textTitleStyle() {
+    return GoogleFonts.openSans(
+        color: Colors.black,
+        fontSize: 26.0,
+        fontWeight: FontWeight.bold,
+    );
+  }
+
+  TextStyle _textDescriptionStyle() {
+    return GoogleFonts.openSans(
+        color: Colors.black,
+        fontSize: 18.0
+    );
+  }
+
+
+}
