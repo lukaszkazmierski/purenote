48a1b96e7c775690392feb577c97338d325302f6
overloadcode <prywatnaapka@gmail.com>
2021-04-06 21:53:47 +0200

    Update settings page

---
+++ lib/domain/models/settings_list_tile_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SettingsListTileModel {
+  final Icon leading;
+  final Widget title;
+  final Widget subtitle;
+
+  const SettingsListTileModel({this.leading, this.title, this.subtitle});
+}
--- lib/presentation/pages/settings_page.dart
+++ lib/presentation/pages/settings_page.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:notebook/service_locator/service_locator.dart';
+import 'package:package_info/package_info.dart';
+import 'package:notebook/domain/models/settings_list_tile_model.dart';
+
+List<SettingsListTileModel> _tiles = [
+  SettingsListTileModel(
+    leading: const Icon(Icons.info, color: Colors.black),
+    title: const Text('About the application'),
+    subtitle: Text(locator.get<PackageInfo>().version)
+  )
+];
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key key}) : super(key: key);
@@ -7,6 +18,22 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
+      body: FractionallySizedBox(
+        widthFactor: 0.9,
+        child: ListView(
+          children: _listTiles
+        ),
+      )
+    );
+  }
+
+  List<Widget> get _listTiles {
+    return List.generate(_tiles.length, (i) =>
+        ListTile(
+          leading: _tiles[i].leading,
+          title: _tiles[i].title,
+          subtitle: _tiles[i].subtitle,
+        )
     );
   }
 }
