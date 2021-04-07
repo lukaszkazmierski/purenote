dd9fb07881a3ca0096abc6a91e8befb59c83a5fc
overloadcode <prywatnaapka@gmail.com>
2021-04-07 10:30:56 +0200

    Redirect to page on tap

--- lib/domain/models/settings_list_tile_model.dart
+++ lib/domain/models/settings_list_tile_model.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:notebook/core/utils/routes/router.gr.dart';
 
 class SettingsListTileModel {
   final Icon leading;
   final Widget title;
   final Widget subtitle;
+  final String redirectToPage;
 
-  const SettingsListTileModel({this.leading, this.title, this.subtitle});
+  const SettingsListTileModel({
+    @required this.leading,
+    @required this.title,
+    @required this.subtitle,
+    this.redirectToPage});
 }
--- lib/presentation/pages/settings_page.dart
+++ lib/presentation/pages/settings_page.dart
@@ -1,13 +1,16 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:notebook/core/utils/routes/router.gr.dart';
 import 'package:notebook/service_locator/service_locator.dart';
 import 'package:package_info/package_info.dart';
 import 'package:notebook/domain/models/settings_list_tile_model.dart';
 
 List<SettingsListTileModel> _tiles = [
   SettingsListTileModel(
-    leading: const Icon(Icons.info, color: Colors.black),
+    leading: const Icon(Icons.info),
     title: const Text('About the application'),
-    subtitle: Text(locator.get<PackageInfo>().version)
+    subtitle: Text(locator.get<PackageInfo>().version),
+    redirectToPage: Routes.aboutApplicationPage
   )
 ];
 
@@ -21,18 +24,19 @@ class SettingsPage extends StatelessWidget {
       body: FractionallySizedBox(
         widthFactor: 0.9,
         child: ListView(
-          children: _listTiles
+          children: _listTiles(context)
         ),
       )
     );
   }
 
-  List<Widget> get _listTiles {
+  List<Widget> _listTiles(BuildContext context) {
     return List.generate(_tiles.length, (i) =>
         ListTile(
           leading: _tiles[i].leading,
           title: _tiles[i].title,
           subtitle: _tiles[i].subtitle,
+          onTap: () => ExtendedNavigator.of(context).push(_tiles[i].redirectToPage),
         )
     );
   }
